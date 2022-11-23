5d2e70ac36decfb93ee74001f2d77850089b6676
Glee-Bunado <gjbunado@agiletech.ph>
2022-11-23 17:01:54 +0800

    updated code

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+import datetime
 
 
 class EstatePropertyOffer(models.Model):
@@ -11,4 +12,18 @@ class EstatePropertyOffer(models.Model):
                               copy=False)
     partner_id = fields.Many2one("res.partner",  required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(string="Validity", default=7)
+    date_deadline = fields.Date(string="Deadline", compute="_validity_date")
+    created_date = fields.Date(default=lambda self: fields.Date.today())
+
+    @api.depends("created_date", "validity")
+    def _validity_date(self):
+        for rec in self:
+            rec.date_deadline = False
+            if rec.created_date:
+                rec.date_deadline = datetime.timedelta(rec.validity) - rec.created_date
+
+
+
+
 
